16b15558f88497a03109ec08f9a3342e5b563a48
Ashkeelun <cbane@unomaha.edu>
2017-02-06 10:16:55 -0600

    Delete admin.py

--- mavfc/Experiment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Experiment)
-admin.site.register(ExperimentRule)
-admin.site.register(Day)
-admin.site.register(ExperimentInstance)
-admin.site.register(UserExperimentInstance)
