77dede5cfc2307e1bd03442a0563f4433abdc52e
Ashkeelun <cbane@unomaha.edu>
2017-02-06 04:30:27 -0600

    Added some utility files
    added some utility files to support creation and deletion of records.

--- mavfc/user/forms.py
+++ mavfc/user/forms.py
@@ -29,7 +29,7 @@ class UserCreationForm(ActivationMailFormMixin, BaseUserCreationForm):
 
     class Meta(BaseUserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name',)
+        fields = ('first_name', 'last_name', 'username', 'email',)
 
     def save(self, **kwargs):
         user = super().save(commit=False)
