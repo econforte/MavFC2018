35cd4ec5ea66efe6941510f483ac8a5562a7d043
Ashkeelun <cbane@unomaha.edu>
2017-03-02 21:50:46 -0600

    updated experiment url file for merge

--- mavfc/experiment/urls.py
+++ mavfc/experiment/urls.py
@@ -25,4 +25,12 @@ urlpatterns = [
     url(r'^rule/(?P<pk>[0-9]+)/update/$', ExperimentRuleUpdate.as_view(), name='experimentrule_update'),
     # ex: /experiment/(experiment.pk)/delete/
     url(r'^rule/(?P<pk>[0-9]+)/delete/$', ExperimentRuleDelete.as_view(), name='experimentrule_delete'),
+    # ex: /experiment/(experiment.pk)/
+    url(r'^instance/(?P<pk>[0-9]+)/$', ExperimentInstanceDetail.as_view(), name='experimentinstance_detail'),
+    # ex: /experiment/create/
+    url(r'^instance/create/$', ExperimentInstanceCreate.as_view(), name='experimentinstance_create'),
+    # ex: /experiment/(experiment.pk)/update/
+    url(r'^instance/(?P<pk>[0-9]+)/update/$', ExperimentInstanceUpdate.as_view(), name='experimentinstance_update'),
+    # ex: /experiment/(experiment.pk)/delete/
+    url(r'^instance/(?P<pk>[0-9]+)/delete/$', ExperimentInstanceDelete.as_view(), name='experimentinstance_delete'),
 ]
