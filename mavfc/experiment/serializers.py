c28dbcb9df18e3d8227dfcbbf025b2d39b91fd66
Ashkeelun <cbane@unomaha.edu>
2017-03-29 00:25:19 -0500

    updated serializers to display all the experiment info.

--- mavfc/experiment/serializers.py
+++ mavfc/experiment/serializers.py
@@ -1,23 +1,33 @@
 from rest_framework import serializers
-from .models import ExperimentInstance
+from .models import ExperimentInstance, Experiment, ExperimentRule
 
 class ExperimentsSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('experiment_name', 'experiment_descr', 'collection_interval')
 
 
-class ExperimentSerializer(serializers.ModelSerializer):
+class ExperimentRuleSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = ExperimentInstance
-        fields = ('id', 'name', 'pi', 'collection_interval', 'experiment_rules')
+        model = ExperimentRule
+        fields = ('id', 'device', 'hour', 'minute', 'baseline_target', 'days')
         depth = 1
+
+
+class ExperimentSerializer(serializers.ModelSerializer):
+    experiment_rules = ExperimentRuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Experiment
+        fields = ('id', 'name', 'pi', 'collection_interval', 'experiment_rules')
+        # depth = 1
 
 
 class ExperimentInstanceSerializer(serializers.ModelSerializer):
-    experiment = ExperimentSerializer
+    experiment = ExperimentSerializer(read_only=True)
 
     class Meta:
         model = ExperimentInstance
-        fields = '__all__'
-        depth = 1
+        fields = ('id', 'start', 'end', 'current', 'experiment')
+        # fields = '__all__'
+        # depth = 1
