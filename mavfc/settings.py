e44c7fea3dade943a5ac817b503950d9a0cd43d9
elocke2017 <hlocke@gmail.com>
2017-04-06 00:10:36 -0500

    updated settings.py DATABASES assignment

--- mavfc/settings.py
+++ mavfc/settings.py
@@ -85,19 +85,18 @@ WSGI_APPLICATION = 'mavfc.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 # Update database configuration with $DATABASE_URL.
 
-#EHL: For Heroku:
+##### EHL: For sqlite db:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+# EHL: For Heroku:
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-# EHL: For sqlite db:
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
+#####
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
