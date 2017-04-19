048e0d55e85a793a88b3ab8a3f11a1ed31801898
elocke2017 <hlocke@gmail.com>
2017-04-19 07:29:34 -0500

    added back 'homepage' to installed apps

--- mavfc/settings.py
+++ mavfc/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'homepage',
     'rest_framework.authtoken',
 ]
 
