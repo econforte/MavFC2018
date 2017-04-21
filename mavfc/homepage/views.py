a44a926c843862135f2824b44ba3772ea650b675
Ashkeelun <cbane@unomaha.edu>
2017-04-20 22:27:03 -0500

    Completed Pi and experiment homepage lists
    #32

--- mavfc/homepage/views.py
+++ mavfc/homepage/views.py
@@ -25,7 +25,7 @@ class Homepage(View):
                 experiments = Experiment.objects.all()
             else:
                 pis = Pi.objects.filter(Q(user=request.user) | Q(experiment__instances__active=True, experiment__instances__instance_users__user__in=[request.user]))
-                experiments = None
+                experiments = Experiment.objects.filter(Q(pi__user__pk=request.user.pk) | Q(instances__instance_users__user__pk=request.user.pk))
             return render(
                 request,
                 'homepage/dashboard.html',
