866c3538937418751c9d414fa64b1fd4607b11a4
DJ Steffensmeier <djsteffy@unomaha.edu>
2017-04-21 02:15:37 -0500

    Revert "Static Files Fix from @Ashkeelun Recommendations"
    This reverts commit 3db2e3838829f321bf6f29983cffc37f7fbb5cde.

--- mavfc/mavfc/settings.py
+++ mavfc/mavfc/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# _ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
@@ -136,7 +136,15 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "media"),
+]
+
+
+
 SITE_ID = 2
+
+
 
 from django.core.urlresolvers import reverse_lazy
 
@@ -194,3 +202,9 @@ verbose = (
 #         },
 #     },
 # }
+
+
+
+
+
+
