4e57a137c5bef071a959bd952fc7eb9815c5999b
Ashkeelun <cbane@unomaha.edu>
2017-04-20 22:56:42 -0500

    Completed Disconnected list
    #32

--- mavfc/homepage/views.py
+++ mavfc/homepage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.views.generic import View
+from datetime import datetime, timedelta
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -23,14 +24,18 @@ class Homepage(View):
             if request.user.is_staff:
                 pis = Pi.objects.all()
                 experiments = Experiment.objects.all()
+                dcPis = Pi.objects.exclude(devices__data__timestamp__gt=datetime.now(tz=None) - timedelta(minutes=60))
             else:
                 pis = Pi.objects.filter(Q(user=request.user) | Q(experiment__instances__active=True, experiment__instances__instance_users__user__in=[request.user]))
                 experiments = Experiment.objects.filter(Q(pi__user__pk=request.user.pk) | Q(instances__instance_users__user__pk=request.user.pk))
+                dcPis = Pi.objects.exclude(devices__data__timestamp__gt=datetime.now(tz=None) - timedelta(minutes=60))\
+                    .filter(Q(user=request.user) | Q(experiment__instances__active=True, experiment__instances__instance_users__user__in=[request.user]))
             return render(
                 request,
                 'homepage/dashboard.html',
                 {'pis': pis,
                  'experiments': experiments,
+                 'dcPis': dcPis,
                  'parent_template': parent_template})
         else:
             return render(request, 'homepage/Justpage.html', {'parent_template': parent_template})
